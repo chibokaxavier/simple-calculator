ef2f24e091e5f1b2ef00d8bdbc1a5e0c6425dc79
manlikexavy <chibokaxavier@gmail.com>
2023-06-02 02:27:43 -0700

    FIRST COMMIT

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,10 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div>
-      
+    <div className='flex justify-center items-center'>
+      <div className='min-h-[500px] h-fit  w-fit min-w-[400px]  shadow-xl mx-auto mt-[100px] rounded-md p-4'>
+
+      </div>
     </div>
     )
 }
